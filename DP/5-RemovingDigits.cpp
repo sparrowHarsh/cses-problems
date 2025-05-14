f3a548afb6213f4c0fa8b78623dd44bd142f15b5
Harsh Vardhan <77070444+sparrowHarsh@users.noreply.github.com>
2025-05-14 18:12:14 +0530

    Added Recursive Approach

--- DP/5-RemovingDigits.cpp
+++ DP/5-RemovingDigits.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define ll long long int
 #define MODE 1000000007LL
 
-
+/*Tabulation Approach */
 void harsh(){
     ll n;cin>>n;
     if(n<10){
@@ -26,6 +26,31 @@ void harsh(){
     }
 
     cout<<dp[n]<<'\n';
+}
+
+/*Recursice Approach */
+ll f(ll n,vector<ll> &dp){
+    if(n == 0) return 0;
+
+    if(dp[n] != -1) return dp[n];
+    ll take = 1e9;
+    string s = to_string(n);
+    for(auto i:s){
+        ll val = i - '0';
+        if(n - val >= 0 && val != 0){
+            take = min(take, 1 + f(n-val,dp));
+        }
+    }
+
+    return dp[n] = take;
+}
+
+
+void harsh(){
+    ll n;cin>>n;
+    vector<ll> dp(n+1,-1);
+    ll ans = f(n,dp);
+    cout<<ans<<'\n';
 }
  
 int main(){
